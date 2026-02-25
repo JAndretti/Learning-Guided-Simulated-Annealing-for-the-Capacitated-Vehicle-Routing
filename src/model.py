fix: Simplify logits masking in CVRPActor by removing redundant masking logic

src/model.py
@@ -434,12 +434,7 @@ class CVRPActor(SAModel):
             logits = torch.ones(n_problems, x.shape[1]).to(self.generator.device)
         else:
             logits = torch.ones(n_problems, x.shape[1]).to(self.generator.device)
-            tmp_mask = (x != 0).squeeze(-1)
-            logits[~tmp_mask] = -float("inf")
-            mask = tmp_mask
-            # Mask first and last logits
-            logits[:, 0] = -float("inf")
-            logits[:, -1] = -float("inf")
+            logits[~mask] = -float("inf")
         c1, _ = self.sample_from_logits(logits, one_hot=False)
 
         logits = torch.ones(n_problems, x.shape[1]).to(self.generator.device)