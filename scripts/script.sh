dcca4ac3b9f13727bbdb7742a557a2c7e4c54f71
JAndretti <jules.andretti@gmail.com>
2026-04-20 18:58:54 +0200

    feat: Update evaluation script to use eval_X_batch.py with multiple seeds

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-uv run python evaluation/eval.py --FOLDER 'BEST' --no-baseline --OUTER_STEPS 1000000 
+uv run python evaluation/eval_X_batch.py --mode "global" --OUTER_STEPS 1000000 --seed 0
 
-uv run python evaluation/eval.py --FOLDER 'BEST' --no-baseline --OUTER_STEPS 500000 --dim 50
+uv run python evaluation/eval_X_batch.py --mode "global" --OUTER_STEPS 1000000 --seed 1
 
-uv run python evaluation/eval.py --FOLDER 'BEST' --no-baseline --OUTER_STEPS 1000000 --dim 50
+uv run python evaluation/eval_X_batch.py --mode "global" --OUTER_STEPS 1000000 --seed 2
+
+uv run python evaluation/eval_X_batch.py --mode "global" --OUTER_STEPS 1000000 --seed 3
+
+uv run python evaluation/eval_X_batch.py --mode "global" --OUTER_STEPS 1000000 --seed 4
+
+uv run python evaluation/eval_X_batch.py --mode "global" --OUTER_STEPS 1000000 --seed 5
+
+uv run python evaluation/eval_X_batch.py --mode "global" --OUTER_STEPS 1000000 --seed 6
+
+uv run python evaluation/eval_X_batch.py --mode "global" --OUTER_STEPS 1000000 --seed 7
+
+uv run python evaluation/eval_X_batch.py --mode "global" --OUTER_STEPS 1000000 --seed 8
+
+uv run python evaluation/eval_X_batch.py --mode "global" --OUTER_STEPS 1000000 --seed 9
+
+uv run python evaluation/eval_X_batch.py --mode "global" --OUTER_STEPS 10000 
