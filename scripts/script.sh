66e3d7f75e538ec4aae9f502d2f9f2ee36a72e99
JAndretti <jules.andretti@gmail.com>
2026-04-21 11:35:38 +0200

    fix: Remove redundant 'python' command from evaluation script

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-uv run python evaluation/eval_X_batch.py --mode "global" --OUTER_STEPS 1000000 --seed 0
+uv run evaluation/eval_X_batch.py --mode "global" --OUTER_STEPS 1000000 --seed 0
 
-uv run python evaluation/eval_X_batch.py --mode "global" --OUTER_STEPS 1000000 --seed 1
+uv run evaluation/eval_X_batch.py --mode "global" --OUTER_STEPS 1000000 --seed 1
 
-uv run python evaluation/eval_X_batch.py --mode "global" --OUTER_STEPS 1000000 --seed 2
+uv run evaluation/eval_X_batch.py --mode "global" --OUTER_STEPS 1000000 --seed 2
 
-uv run python evaluation/eval_X_batch.py --mode "global" --OUTER_STEPS 1000000 --seed 3
+uv run evaluation/eval_X_batch.py --mode "global" --OUTER_STEPS 1000000 --seed 3
 
-uv run python evaluation/eval_X_batch.py --mode "global" --OUTER_STEPS 1000000 --seed 4
+uv run evaluation/eval_X_batch.py --mode "global" --OUTER_STEPS 1000000 --seed 4
 
-uv run python evaluation/eval_X_batch.py --mode "global" --OUTER_STEPS 1000000 --seed 5
+uv run evaluation/eval_X_batch.py --mode "global" --OUTER_STEPS 1000000 --seed 5
 
-uv run python evaluation/eval_X_batch.py --mode "global" --OUTER_STEPS 1000000 --seed 6
+uv run evaluation/eval_X_batch.py --mode "global" --OUTER_STEPS 1000000 --seed 6
 
-uv run python evaluation/eval_X_batch.py --mode "global" --OUTER_STEPS 1000000 --seed 7
+uv run evaluation/eval_X_batch.py --mode "global" --OUTER_STEPS 1000000 --seed 7
 
-uv run python evaluation/eval_X_batch.py --mode "global" --OUTER_STEPS 1000000 --seed 8
+uv run evaluation/eval_X_batch.py --mode "global" --OUTER_STEPS 1000000 --seed 8
 
-uv run python evaluation/eval_X_batch.py --mode "global" --OUTER_STEPS 1000000 --seed 9
+uv run evaluation/eval_X_batch.py --mode "global" --OUTER_STEPS 1000000 --seed 9
 
-uv run python evaluation/eval_X_batch.py --mode "global" --OUTER_STEPS 10000 
+uv run evaluation/eval_X_batch.py --mode "global" --OUTER_STEPS 10000 
